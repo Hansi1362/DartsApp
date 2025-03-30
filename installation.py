257c891663453eeb97153fc232469848da871d26
Hansi1362 <helgetasche@gmail.com>
2025-03-30 18:23:33 +0200

    Update installation.py

--- installation.py
+++ installation.py
@@ -5,7 +5,6 @@ target_file = os.path.abspath("PressHereToStartTheApp")
 desktop = winshell.desktop()
 shortcut_path = os.path.join(desktop, "DartsApp2.0.lnk")
 
-# Create the shortcut
 with winshell.shortcut(shortcut_path) as shortcut:
     shortcut.path = target_file
     shortcut.description = "Shortcut to DartsApp2.0"
